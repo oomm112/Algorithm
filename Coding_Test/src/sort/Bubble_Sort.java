a187f8ae1b03af88e64b953154d8c853ce71f061
Minhuyk <rumsk04622@gmail.com>
2021-06-09 10:25:13 +0900

    21/06/09 Java알고리즘 풀이
    첫번째 커밋

---
+++ Coding_Test/src/sort/Bubble_Sort.java
@@ -0,0 +1,27 @@
+package sort;
+
+import java.util.Arrays;
+
+public class Bubble_Sort {	//직관적이며 코드가 짧고 단순하나, 비효율적인 정렬 방식이므로 피해야 한다.
+	public static int[] bubble(int[] a, int len){	//
+		for (int i = len; i > 0; i--) {	// i가 0보다 클경우 i--
+			for (int j = 0; j < i-1; j++) { // j가 i보다 작을경우 ++
+				if (a[j] > a[j+1]) { // a[j]번째가 a[j+1]보다 클경우
+					int t = a[j];	//t 는 a[j]번쨰로 대입
+					a[j] = a[j+1];	//a[j]를 a[j+1]로 대입
+					a[j+1] = t;	//a[j+1] 을 t로 대입
+					System.out.println(j+"번쨰"+a[j]+" " +(j+1)+"번째"+a[j+1]);
+				}
+			}
+		}
+		return a;
+	}
+	
+	public static void main(String[] args) {
+		int[] a = {4,2,5,1,3};	//배열
+		int len = a.length;
+		
+		bubble(a, len);
+		System.out.println(Arrays.toString(a));
+	}
+}
---
+++ Coding_Test/src/sort/Cocktail_Sort.java
@@ -0,0 +1,43 @@
+package sort;
+
+import java.util.Arrays;
+
+public class Cocktail_Sort { // 버블정렬의 단점을 개선한 정렬 알고리즘, 처음과 끝을 왕복하며 정렬하는 구조 .정렬 하는모습이 칵테일같아서 칵테일정렬이다.
+	public static int[] Cocktail(int[] arr) {
+		int begin = 0;
+		int end = arr.length-1;
+		boolean is_swapped = true;
+		int t = 0;
+
+		while (is_swapped) {
+			is_swapped = false;
+			for (int i = begin; i < end; i++) {
+				if (arr[i] > arr[i + 1]) {
+					t = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = t;
+					is_swapped = true;
+					System.out.println("앞부터정렬"+i+"번쨰"+arr[i]+" " +(i+1)+"번째"+arr[i+1]);
+				}
+			}
+			if (!is_swapped) {break;}
+			
+			for (int i = end; i > begin; --i) {
+				if (arr[i] > arr[i + 1]) {
+					t = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = t;
+					is_swapped = true;
+					System.out.println("뒤부터정렬"+i+"번쨰"+arr[i]+" " +(i+1)+"번째"+arr[i+1]);
+				}
+			}
+		}
+		return arr;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 2, 5, 4, 1, 3 };
+		Cocktail(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+}
