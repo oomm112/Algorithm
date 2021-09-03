a08eccc526248b7875a1b98680d73fd7fc0d9057
Minhuyk <rumsk04622@gmail.com>
2021-09-03 18:45:17 +0900

    solved.ac패키지
    solved.ac형식으로 패키지변경
    09/03

--- Coding_Test/src/backjoon_input_output/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package backjoon_input_output;
-
-public class HelloWorld {
-	public static void main(String args[]){
-        System.out.println("Hello World!");
-    }
-}
--- Coding_Test/src/backjoon_input_output/We_love_kriii.java
+++
@@ -1,9 +0,0 @@
-package backjoon_input_output;
-
-public class We_love_kriii {
-	public static void main(String[] args) {
-		for (int i = 0; i < 2; i++) {		
-			System.out.println("강한친구 대한육군");
-		}
-	}
-}
