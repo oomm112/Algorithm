82372834833a81c35fee7505a0d9a686a7e8a52a
Minhuyk <rumsk04622@gmail.com>
2021-09-16 22:46:02 +0900

    09/16

---
+++ Coding_Test/src/solvedClass2/Deck_10866.java
@@ -0,0 +1,21 @@
+package solvedClass2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Deck_10866 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			
+		}
+	}
+}
