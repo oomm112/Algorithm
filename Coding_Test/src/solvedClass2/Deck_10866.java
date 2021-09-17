a7ff465b049af1bfb7e71417cca9b14ea4e58510
Minhuyk <rumsk04622@gmail.com>
2021-09-17 20:24:03 +0900

    09/17

--- Coding_Test/src/solvedClass2/Deck_10866.java
+++ Coding_Test/src/solvedClass2/Deck_10866.java
@@ -5,6 +5,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Deck_10866 {
@@ -12,10 +14,70 @@ public class Deck_10866 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
+		Deque<Integer> deque = new ArrayDeque<Integer>();
 		int N = Integer.parseInt(br.readLine());
-		
+
 		for (int i = 0; i < N; i++) {
-			
+			String[] line = br.readLine().split(" ");
+			switch (line[0]) {
+				case "push_front": {
+					deque.addFirst(Integer.parseInt(line[1]));
+					break;
+				}
+				case "push_back" : {
+					deque.addLast(Integer.parseInt(line[1]));
+					break;
+				}
+				case "pop_front" : {
+					if (deque.isEmpty()) {
+						bw.write("-1"+"\n");
+					}else {
+						bw.write(deque.peek()+"\n");
+						deque.removeFirst();
+					}
+					break;
+				}
+				case "pop_back" : {
+					if (deque.isEmpty()) {
+						bw.write("-1"+"\n");
+					}else {
+						bw.write(deque.peekLast()+"\n");
+						deque.removeLast();
+					}
+					break;
+				}
+				case "size" : {
+					bw.write(deque.size()+"\n");
+					break;
+				}
+				case "empty" : {
+					if (deque.isEmpty()) {
+						bw.write("1"+"\n");
+					}else {
+						bw.write("0"+"\n");
+					}
+					break;
+				}
+				case "front" : {
+					if (deque.isEmpty()) {
+						bw.write("-1"+"\n");
+					}else {
+						bw.write(deque.getFirst()+"\n");
+					}
+					break;
+				}
+				case "back" : {
+					if (deque.isEmpty()) {
+						bw.write("-1"+"\n");
+					}else {
+						bw.write(deque.getLast()+"\n");
+					}
+					break;
+				}
+			}
 		}
+		br.close();
+		bw.flush();
+		bw.close();
 	}
 }
