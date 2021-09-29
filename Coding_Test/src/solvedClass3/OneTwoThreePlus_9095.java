99367435fd032ba2f322d37022aa7b6d56de7d18
Minhuyk <rumsk04622@gmail.com>
2021-09-29 23:44:07 +0900

    09/29

--- Coding_Test/src/solvedClass3/OneTwoThreePlus_9095.java
+++ Coding_Test/src/solvedClass3/OneTwoThreePlus_9095.java
@@ -6,20 +6,19 @@ import java.io.InputStreamReader;
 
 public class OneTwoThreePlus_9095 {
 	static Integer[] DP;
+	//DP방식 이용
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int T = Integer.parseInt(br.readLine());
 		DP = new Integer[11];
 
-		DP[0] = 0;
-		DP[1] = 1;
+		DP[0] = 0;	//0일때는 만들수 없으므로 0
+		DP[1] = 1;	// (1)
+		DP[2] = 2;	// (1+1), (2)
+		DP[3] = 4;	// (1+1+1) , (1+2),(2+1), (2+2)
 
-		solveDP(2);
-		
-		for (int i = 0; i < DP.length; i++) {
-			System.out.println(DP[i]);
-		}
+		solveDP(4);
 		
 		while(T-- > 0) {
 			int n = Integer.parseInt(br.readLine());
@@ -30,8 +29,12 @@ public class OneTwoThreePlus_9095 {
 	static int solveDP(int num) {
 		if (num > DP.length-1) {
 			return 0;
-		}
-		DP[num] = DP[num-1] + (num-1);
+		}	
+		
+		//점화식 
+		//* EX) num = 4일 경우 4는  3의 경우의수 + 2의경우의수 + 1의경우의수 이다.
+		// 마찬가지로 5는 4 + 3 +2 의 경우의수 들의 합이다.
+		DP[num] = DP[num-1] + DP[num-2] + DP[num-3];
 		
 		return solveDP(num+1);
 	}
