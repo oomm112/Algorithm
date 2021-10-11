55fa5d3924e4a7677bfaba34597b9cf123ce45b0
Minhuyk <rumsk04622@gmail.com>
2021-10-12 00:48:38 +0900

    10/12

--- Coding_Test/src/solvedClass3/Remote_1107.java
+++ Coding_Test/src/solvedClass3/Remote_1107.java
@@ -3,37 +3,57 @@ package solvedClass3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Remote_1107 {
-	static ArrayList<Integer> canButton = new ArrayList<Integer>();		//누를수 있는 버튼의 배열
-	static int M,now;
-	static String N;
+	static int N,M;
+	static boolean[] broken;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
-		N = br.readLine();		//이동하고자 하는 채널
+
+		N = Integer.parseInt(br.readLine()); 	//이동하고자 하는 채널
 		M = Integer.parseInt(br.readLine()); 		//고장난 버튼의 개수
-		now = 100;									//현재 수빈이가 보고있는 채널
-				
-		st = new StringTokenizer(br.readLine());
-		while (M-- > 0) {
-			int x = Integer.parseInt(st.nextToken());
-			
-			
-		}
-		
-		if (Integer.parseInt(N) == now) {
-			System.out.println("0");
-		}else {
-			
+		broken = new boolean[10];
+
+
+		//고장난 버튼을 broken에 담아준다.
+		if (M != 0) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				int x = Integer.parseInt(st.nextToken());
+
+				broken[x] = true;
+			}
 		}
 
+		int result = Math.abs(N-100);	// 번호를 누르는 경우를 제외한 +,-만 누를경우
+		for (int i = 0; i <= 1000000; i++) {
+			int chk = solve(i);
+			if (chk > 0) {
+				int press = Math.abs(N - i);		//+와 - 버튼을 누르는 횟수
+				result = Math.min(result, press + chk);
+			}
+		}
+		System.out.println(result);
 	}
-	
-	static void solve() {
 
+	static int solve(int num) {
+		if (num == 0) {
+			if (broken[0]) {
+				return 0;
+			}else {
+				return 1;
+			}
+		}
+		int len = 0;
+		while(num > 0) {
+			if (broken[num%10]) {
+				return 0;
+			}
+			num /= 10;
+			len += 1;
+		}
+		return len;
 	}
 }
