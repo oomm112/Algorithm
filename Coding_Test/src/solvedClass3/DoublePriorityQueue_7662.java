d74595750c655ea2e930e5875cf2b4b96d77f079
Minhuyk <rumsk04622@gmail.com>
2021-10-13 01:20:51 +0900

    10/13

--- Coding_Test/src/solvedClass3/DoublePriorityQueue_7662.java
+++ Coding_Test/src/solvedClass3/DoublePriorityQueue_7662.java
@@ -1,9 +1,14 @@
 package solvedClass3;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
 public class DoublePriorityQueue_7662 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<E>
 
 	}
 
