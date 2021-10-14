b8620191f067067eed54a1e8b5abc37d84f60f41
Minhuyk <rumsk04622@gmail.com>
2021-10-14 23:33:30 +0900

    10/14

---
+++ Coding_Test/src/programmers_lvl1/StringCompression.java
@@ -0,0 +1,24 @@
+package programmers_lvl1;
+
+public class StringCompression {
+
+	public static void main(String[] args) {
+		solution("aabbaccc");
+	}
+	public static int solution(String s) {
+		int answer = 0;
+		for (int i = 1; i <= s.length()/2; i++) {
+			int comp = compress(s, i).length();
+		}
+		return answer;
+	}
+	public static String compress(String s, int i) {
+		int count = 1;
+		String pattern = "";
+		for (int j = 0; j < s.length() + i; j+=1) {
+			SS
+		}
+		
+		return s;
+	}
+}
