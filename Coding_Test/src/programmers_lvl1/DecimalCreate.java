1386bb5c545e6297f6e931b14f38ae2709521389
Minhuyk <rumsk04622@gmail.com>
2021-10-16 23:20:57 +0900

    10/16

--- Coding_Test/src/programmers_lvl1/DecimalCreate.java
+++ Coding_Test/src/programmers_lvl1/DecimalCreate.java
@@ -2,17 +2,37 @@ package programmers_lvl1;
 
 public class DecimalCreate {
 	public static int solution(int[] nums) {
-        int answer = -1;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+        int answer = 0;
+        int result = 0;		//세수를 더한 값
+        
+        for (int i = 0; i < nums.length-2; i++) {
+        	for (int j = i+1; j < nums.length-1; j++) {
+				for (int j2 = j+1; j2 < nums.length; j2++) {
+					result = (nums[i] + nums[j] + nums[j2]);
+					if (sosu(result)) {
+						answer++;
+					}
+				}
+			}
+		}
+        
         return answer;
     }
-
+	public static boolean sosu(int num) {
+		boolean chk = true;
+		
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				chk = false;
+				break;
+			}
+		}
+		return chk;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] nums = {1, 2, 7, 6, 4};
+		System.out.println(solution(nums));
 	}
 
 }
